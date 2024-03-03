e0dd2afd21b3c5f07bbbf7b8b2d7e8697858aa21
Marcel Domingus <domingux@xs4all.nl>
2024-03-03 13:49:45 +0100

    Add delete_non_english_meta_json_files

--- prep_for_obsidian.py
+++ prep_for_obsidian.py
@@ -14,5 +14,15 @@ def delete_non_english_mdx_files(directory):
                 os.remove(file)
 
 
+def delete_non_english_meta_json_files(directory):
+    # Traverse all folders and subfolders
+    for root, dirs, files in os.walk(directory):
+        # Search for files with .mdx extension
+        for file in glob.glob(root + "/_meta.*.json"):
+            # Delete all files whose file names does not end in 'en.mdx'
+            if not file.endswith("_meta.en.json"):
+                os.remove(file)
+
 if __name__ == "__main__":
     delete_non_english_mdx_files(".")
+    delete_non_english_meta_json_files(".")
