dba663e896c6d454d27420d6c6ec23f98d5727d7
Marcel Domingus <domingux@xs4all.nl>
2024-03-03 13:43:39 +0100

    Add prep_for_obsidian.py

---
+++ prep_for_obsidian.py
@@ -0,0 +1,18 @@
+# Traverse all folders and subfolder and search for files with .mdx extension and delete all files whose file names does not end in 'en.mdx'
+# This is to remove all non-english files from the folder
+import os
+import glob
+
+
+def delete_non_english_mdx_files(directory):
+    # Traverse all folders and subfolders
+    for root, dirs, files in os.walk(directory):
+        # Search for files with .mdx extension
+        for file in glob.glob(root + "/*.mdx"):
+            # Delete all files whose file names does not end in 'en.mdx'
+            if not file.endswith("en.mdx"):
+                os.remove(file)
+
+
+if __name__ == "__main__":
+    delete_non_english_mdx_files(".")
